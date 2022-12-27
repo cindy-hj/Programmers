c0759014aa277c6640a1804cdedb4ec462cff831
cindy-hj <120145140+cindy-hj@users.noreply.github.com>
2022-12-27 20:40:51 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.19 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.js
@@ -0,0 +1,9 @@
+function solution(numbers) {
+    var answer = [];
+    for(let i = 0; i < numbers.length; i++) {
+        answer.push(numbers[i]*2); // 배열에 원소를 넣어주는 method
+        // 반복문인것 같긴한데 어디에 돌려야 될지 헷갈리면 일단 쭉 나열해보자
+    }
+    return answer;
+}
+ 
